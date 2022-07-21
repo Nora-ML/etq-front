26e9167a55329ceba823e20b14e3ce12aff71e0f
Nora-ML <86887904+Nora-ML@users.noreply.github.com>
2022-07-21 22:34:24 +0200

    adding api key

--- src/requests/index.js
+++ src/requests/index.js
@@ -201,11 +201,14 @@ export const fetchUser = async (email) => {
 
 export const itemsInCategory = async (categoryName, limit, skip) => {
 	console.log("Index.js ---- itemsInCategory() limit:", limit, "Skip :", skip);
+
 	try {
 		const response = await fetch(
 			`${API}/shop/${categoryName}?limit=${limit}&skip=${skip}`,
 			{
 				method: "GET",
+				"api-key":
+					"NgY1FgiHqSQnV1gpY1F1pXqDoQu4iqY6OXNDY8BJ29llEw5itewzEI7bkcAfo64N",
 			}
 		);
 		return await response.json();

f14db493ee857c83c6161708e4ce725f095665b1
Nora-ML <86887904+Nora-ML@users.noreply.github.com>
2023-05-07 16:27:52 +0300

    update react render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import MiniContextProvider from "./context/miniNavContext";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+	<StrictMode>
+		<MiniContextProvider>
+			<App />
+		</MiniContextProvider>
+	</StrictMode>
+);
+/* import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import MiniContextProvider from "./context/miniNavContext";
@@ -11,3 +25,4 @@ ReactDOM.render(
 	</StrictMode>,
 	document.getElementById("root")
 );
+ */
